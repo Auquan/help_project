d1742c1657cf420beca21434d29f141befa19bb9
Shub Jain <shubham1217@gmail.com>
2020-05-18 19:13:08 +0530

    Intial Repository setup with CI, linting and tests (#1)
    * Setup commits
    
    * Add ci config with test
    
    * Add travis ci
    
    * fix requirements import
    
    * Minor fixes
    
    * Remove gitlab ci
    
    Co-authored-by: shub <shub@auquan.com>

---
+++ test/economic_model/sample_economic_model/test_sample_economic_model.py
@@ -0,0 +1,7 @@
+""" Test for sample economic model """
+
+import pytest # pylint: disable=unused-import
+
+def test_sample_passing_method():
+    """ Passing Test for a sample function """
+    assert True
